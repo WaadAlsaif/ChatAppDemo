1f6060bd09f494b79525f6e397fc5e0397b254e5
WAAD ALSAIF <50026099+WaadAlsaif@users.noreply.github.com>
2022-08-29 17:07:16 +0300

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Waad Alsaif on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
